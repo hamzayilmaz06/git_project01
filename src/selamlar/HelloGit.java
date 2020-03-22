6e2c53950fdfdad5f4047fc7731cc44843576550
hamzayilmaz06 <62487867+hamzayilmaz06@users.noreply.github.com>
2020-03-22 03:32:18 +0300

    Sildim

--- src/selamlar/HelloGit.java
+++ src/selamlar/HelloGit.java
@@ -3,12 +3,7 @@ package selamlar;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Merhaba Githup");
-		System.out.println("Yeniden Merhaba Githup");
-		System.out.println("3. Kez Merhaba !");
-		System.out.println("4. Kez Merhaba !");
-		System.out.println("save");
-
+		System.out.println("Sildim");
 	}
 
 }
