979024a92637112b1128e04b5e334e0bf301afd8
davuterarslan <compengi62@gmail.com>
2020-03-22 03:16:27 +0300

    3. Kez

--- src/selamlar/HelloGit.java
+++ src/selamlar/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Merhaba Githup");
 		System.out.println("Yeniden Merhaba Githup");
+		System.out.println("3. Kez Merhaba !");
 
 	}
 
