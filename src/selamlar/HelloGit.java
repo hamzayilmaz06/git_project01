51355cca727bead6716705665cebdceb7e887865
davuterarslan <compengi62@gmail.com>
2020-03-22 03:25:53 +0300

    save

--- src/selamlar/HelloGit.java
+++ src/selamlar/HelloGit.java
@@ -7,6 +7,7 @@ public class HelloGit {
 		System.out.println("Yeniden Merhaba Githup");
 		System.out.println("3. Kez Merhaba !");
 		System.out.println("4. Kez Merhaba !");
+		System.out.println("save");
 
 	}
 
